c85ed8806726d9a38e65a7ce4cf3f5eb7e51a074
c-santana-sp <eduardomarquesdesantana@gmail.com>
2025-05-26 19:22:16 -0300

    feat: main application class

---
+++ src/main/java/com/movingpack/movingpack/MovingPackApplication.java
@@ -0,0 +1,13 @@
+package com.movingpack.movingpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingPackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovingPackApplication.class, args);
+	}
+
+}
