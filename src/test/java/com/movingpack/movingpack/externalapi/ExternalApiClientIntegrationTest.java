eb8479f3e8bae6d14bff82b23dd1a718788a6a68
c-santana-sp <eduardomarquesdesantana@gmail.com>
2025-05-26 19:18:09 -0300

    test: external api client tests

---
+++ src/test/java/com/movingpack/movingpack/externalapi/ExternalApiClientIntegrationTest.java
@@ -0,0 +1,50 @@
+package com.movingpack.movingpack.externalapi;
+
+import com.movingpack.movingpack.config.TestcontainersConfiguration;
+import com.movingpack.movingpack.externalapi.dto.ExternalApiCepResult;
+import com.movingpack.movingpack.externalapi.dto.ExternalApiResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ContextConfiguration(initializers = TestcontainersConfiguration.Initializer.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ExternalApiClientIntegrationTest {
+
+    @Autowired
+    private ExternalApiClient externalApiClient;
+
+    @Test
+    void shouldReturnMockedSuccessResponse() {
+        String cep = "03258060";
+        ExternalApiResponse response = externalApiClient.fetchCep(cep);
+
+        assertEquals(200, response.statusCode());
+        assertInstanceOf(ExternalApiCepResult.Success.class, response.result());
+    }
+
+    @Test
+    void shouldReturnMockedNotFoundFailureResponse() {
+        String cep = "11111111";
+        ExternalApiResponse response = externalApiClient.fetchCep(cep);
+
+        assertEquals(404, response.statusCode());
+        assertInstanceOf(ExternalApiCepResult.Failure.class, response.result());
+    }
+
+    @Test
+    void shouldReturnMockedUnknownFailureResponse() {
+        String cep = "00000000";
+        ExternalApiResponse response = externalApiClient.fetchCep(cep);
+
+        assertEquals(500, response.statusCode());
+    }
+}
